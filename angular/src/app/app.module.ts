f32e4d32e95a7a30ef0f403bc42ea26af225ab41
Kohichiro W Narita <kwnarita@jp.ibm.com>
2017-07-15 18:34:34 +0900

    bootstrap initial

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Ng2BootstrapModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,7 @@ import { ContentComponent } from './content.component';
     AppComponent, ContentComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule
+    BrowserModule, AppRoutingModule, Ng2BootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
